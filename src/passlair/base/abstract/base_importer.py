695009b1bad2c3bf0362bef702f88658d630b642
Adam Sarga <vronst@gmail.com>
2026-08-22 13:08:02 +0200

    chore: started working on importer

---
+++ src/passlair/base/abstract/base_importer.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class BaseImporter(ABC):
+
+    @abstractmethod
+    def import_from_file(self, file: str, fmt: str) -> None:
+        pass
+
+    @abstractmethod
+    def import_from_clipboard(self, fmt: str) -> None:
+        pass
--- src/passlair/share/importer.py
+++ src/passlair/share/importer.py
@@ -1,1 +1,7 @@
-# TODO
+import json
+import pyperclip
+
+from passlair.base.abstract.base_importer import BaseImporter
+
+class Importer(BaseImporter):
+    pass
