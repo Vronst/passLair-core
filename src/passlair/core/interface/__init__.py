d544ca176827b8551efcf18414a29ebcf3112af8
Adam Sarga <vronst@gmail.com>
2026-09-21 21:12:09 +0200

    fix: exposed facads

--- src/passlair/core/interface/__init__.py
+++ src/passlair/core/interface/__init__.py
@@ -1,4 +1,5 @@
 from .identity import Identity
 from .password_manager import PasswordManager
+from .synced_dual_databases import SyncedDualDatabases
 
-__all__ = ["Identity", "PasswordManager"]
+__all__ = ["Identity", "PasswordManager", "SyncedDualDatabases"]
